da715e33a771838f31c4542af9bcc227ced4e575
rito <ghdtkals15@naver.com>
2020-06-21 19:45:26 +0900

    init

---
+++ CodingTestPracticeCPP/CodingTestPracticeCPP/20200621_1.cpp
@@ -0,0 +1,52 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42576
+// 해시 - 완주하지 못한 선수
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion)
+{
+	map<string, int> partMap;
+
+	// 참가 목록에서 확인하고 맵에 추가
+	for (auto iter = participant.begin(); iter != participant.end(); iter++)
+	{
+		auto target = partMap.find(*iter);
+
+		// 존재하지 않으면 1로 추가
+		if (target == partMap.end())
+		{
+			partMap.insert(make_pair(*iter, 1));
+		}
+		// 존재하면 +1
+		else
+		{
+			target->second += 1;
+		}
+	}
+
+	// 완주자 목록에서 확인하고 맵에서 감소
+	for (auto iter = completion.begin(); iter != completion.end(); iter++)
+	{
+		auto target = partMap.find(*iter);
+
+		// 존재하면 -1
+		if (target != partMap.end())
+		{
+			if (target->second == 1)
+			{
+				partMap.erase(target);
+			}
+			else
+			{
+				target->second -= 1;
+			}
+		}
+	}
+
+	return partMap.begin()->first;
+}
